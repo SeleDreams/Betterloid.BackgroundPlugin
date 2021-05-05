47a4abb639d08fdedd0b686637d1db3dad459ee7
SeleDreams <seledreams.contact@gmail.com>
2021-05-05 10:55:01 +0200

    Initial commit

---
+++ PianorollBackground/BackgroundConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundPlugin
+{
+    public class BackgroundConfig
+    {
+        public string CustomBackground { get; set; } = "";
+        public string VerticalAlignment { get; set; } = "Center";
+        public string HorizontalAlignment { get; set; } = "Center";
+        public string Stretch { get; set; } = "UniformToFill";
+        public float Opacity { get; set; } = 0.5f;
+    }
+}
